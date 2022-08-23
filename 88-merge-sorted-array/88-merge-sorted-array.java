81e23f8a07cfdfafa810b2b35aaca7beb01f7e8f
Aadil Ansari <ansariaadil611@gmail.com>
2022-08-23 10:21:38 +0530

    Time: 0 ms (100.00%), Space: 43 MB (44.11%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,25 +1,13 @@
 class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i1=0,i2=0;
-        vector<int> res;
-        
-        while(i1 < m && i2 < n){
-            if(nums1[i1] <= nums2[i2])
-            {
-                res.push_back(nums1[i1]);
-                i1++;
-            }
-            else{
-                res.push_back(nums2[i2]);
-                i2++;
-            }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i1=m-1,i2=n-1,i=n+m-1;
+        while(i1>=0 && i2>=0){
+            if(nums1[i1] >= nums2[i2])
+                nums1[i--] = nums1[i1--];
+            else
+                nums1[i--] = nums2[i2--];
         }
-        for(;i1<m;i1++)
-            res.push_back(nums1[i1]);
-        for(;i2<n;i2++)
-            res.push_back(nums2[i2]);
-        
-        nums1 = res;
+        while(i2>=0 && i>=0)
+            nums1[i--] = nums2[i2--];
     }
-};
+}
